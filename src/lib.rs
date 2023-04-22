d66cd519352e75334b054ca95fbdf428f6581fcf
qscqesze <475517977@qq.com>
2023-04-22 16:09:11 +0800

    add lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,51 @@
 pub mod sdk;
 pub mod services;
-/*
 use services::dysmsapi;
 use gostd::strings;
 
+/*
 const AliyunSmsServerRegion: &str = "cn-hangzhou";
 // 这个key已经废弃了，不用试了。
-const AliyunSmsAccessKeyID: &str = "************";
-const AliyunSmsAccessKeySecret: &str = "**********"; 
+const AliyunSmsAccessKeyID: &str = "xxxxxx";
+const AliyunSmsAccessKeySecret: &str = "xxxxxx"; 
 const AliyunSmsReportTempleateCode: &str = "SMS_154950909"; // 通知模版
 const AliyunSmsSignName: &str = "阿里云短信测试"; // 短信署名
 const AliyunSmsTemplateParam: &str = "{\"code\":\"1234\"}";
 
 fn main() {
-    let phoneNumber="13408500362";
-    let res = SendSMS(phoneNumber);
+    let res = SingleSendMail();
     println!("res: {:?}", res);
+}
+
+fn SingleSendMail() -> Result<(), std::io::Error> {
+    let mut client = dysmsapi::Client::NewClientWithAccessKey(
+        AliyunSmsServerRegion,
+        AliyunSmsAccessKeyID,
+        AliyunSmsAccessKeySecret,
+    )?;
+    let mut request = dysmsapi::SingleSendMailRequest();
+    request.AccountName = "idlejianghu@surprising.studio".to_string();
+    request.ToAddress = "475517977@qq.com".to_string();
+    request.Subject = "验证码".to_string();
+    request.TextBody = "验证码为123456, 有效期10分钟。".to_string();
+    println!("request: {:?}", &request);
+    let response = client.SingleSendMail(&mut request);
+    println!("response: {:?}", &response);
+    Ok(())
+}
+
+fn CreateDomain(domain: &str) -> Result<(), std::io::Error> {
+    let mut client = dysmsapi::Client::NewClientWithAccessKey(
+        AliyunSmsServerRegion,
+        AliyunSmsAccessKeyID,
+        AliyunSmsAccessKeySecret,
+    )?;
+    let mut request = dysmsapi::CreateDomainRequest();
+    request.DomainName = domain.to_owned();
+    println!("request: {:?}", &request);
+    let response = client.CreateDomain(&mut request);
+    println!("response: {:?}", &response);
+    Ok(())
 }
 
 fn SendSMS(phoneNumber: &str) -> Result<(), std::io::Error> {
--- src/services/dysmsapi/alimail.rs
+++ src/services/dysmsapi/alimail.rs
@@ -17,6 +17,15 @@ impl Client {
         response = serde_json::from_slice(&baseResponse.httpContentBytes)?;
         Ok(response)
     }
+    pub fn SingleSendMail(&mut self, request: &mut SingleSendMailRequest)
+    -> Result<SingleSendMailResponse, Error> {
+        let mut response = SingleSendMailResponse();
+        request.BuildQueryParams();
+        let mut baseResponse = responses::BaseResponse::default();
+        self.DoAction(&mut request.rpcRequest, &mut baseResponse)?;
+        response = serde_json::from_slice(&baseResponse.httpContentBytes)?;
+        Ok(response)
+    }
 }
 
 use crate::sdk::requests::BaseRequestExt;
@@ -63,3 +72,56 @@ pub fn CreateDomainRequest() -> CreateDomainRequest {
 pub fn CreateDomainResponse() -> CreateDomainResponse {
     CreateDomainResponse::default()
 }
+
+///////////////////////// send mail
+#[derive(Default, Debug)]
+pub struct SingleSendMailRequest {
+    rpcRequest: requests::RpcRequest,
+    pub AccountName: String,
+    pub ToAddress: String,
+    pub Subject: String,
+    pub TextBody: String,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct SingleSendMailResponse {
+    // baseResponse: responses::BaseResponse,
+    pub EnvId: String,
+    pub RequestId: String,
+}
+
+impl BaseRequestExt for SingleSendMailRequest {
+    fn base(&self) -> &BaseRequest {
+        self.rpcRequest.base()
+    }
+
+    fn base_as_mut(&mut self) -> &mut BaseRequest {
+        self.rpcRequest.base_as_mut()
+    }
+}
+
+impl SingleSendMailRequest {
+    pub fn BuildQueryParams(&mut self) {
+        self.addQueryParam("AccountName", &self.AccountName.to_owned());
+        self.addQueryParam("AddressType", &"1");
+        self.addQueryParam("ReplyToAddress", &"true");
+        self.addQueryParam("ToAddress", &self.ToAddress.to_owned());
+        self.addQueryParam("Subject", &self.Subject.to_owned());
+        self.addQueryParam("TextBody", &self.TextBody.to_owned());
+    }
+}
+
+pub fn SingleSendMailRequest() -> SingleSendMailRequest {
+    let mut request = SingleSendMailRequest::default();
+    request
+        .rpcRequest
+        .InitWithApiInfo("Dm", "2015-11-23", "SingleSendMail", "", "");
+    request.SetMethod(requests::POST);
+    request
+}
+
+pub fn SingleSendMailResponse() -> SingleSendMailResponse {
+    SingleSendMailResponse::default()
+}
+
+
